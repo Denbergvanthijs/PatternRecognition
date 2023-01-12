4ba0b202aebd9a0ade9a2005f70200fb509471dd
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-01-12 17:13:31 +0100

    Added a 256 script

--- greyscaling256x256.py
+++ greyscaling256x256.py
@@ -26,6 +26,13 @@ ds_train = ds_train.map(lambda x: tf.cast(x, tf.float32) / 255.0)
 ds_val = ds_val.map(lambda x: tf.cast(x, tf.float32) / 255.0)
 ds_test = ds_test.map(lambda x: tf.cast(x, tf.float32) / 255.0)
 
+# Reshape images to 256x256
+# This is needed when evaluation on Places365 with the GAN model
+# See https://github.com/ImagingLab/Colorizing-with-GANs/issues/4#issuecomment-407769051
+ds_train = ds_train.map(lambda x: tf.image.resize_with_pad(x, 256, 256))
+ds_val = ds_val.map(lambda x: tf.image.resize_with_pad(x, 256, 256))
+ds_test = ds_test.map(lambda x: tf.image.resize_with_pad(x, 256, 256))
+
 # Convert test dataset to greyscale
 ds_train_grey_np = ds_train.map(lambda x: greyscale_tf(x))
 ds_val_grey_np = ds_val.map(lambda x: greyscale_tf(x))
@@ -53,39 +60,25 @@ plt.show()
 
 # Save dataset to two seperate numpy arrays
 # This is to save time when running the eventual colorisation scripts
-if not os.path.exists('./data/'):
-    os.makedirs('./data/')
-
+if not os.path.exists('./data/256'):
+    os.makedirs('./data/256')
 for name, dataset in zip(("train", "val", "test"), (ds_train_combined, ds_val_combined, ds_test_combined)):
     images, images_grey = zip(*dataset.as_numpy_iterator())
-    np.save(f'./data/{name}_images.npy', np.array(images))
-    np.save(f'./data/{name}_images_grey.npy', np.array(images_grey))
+    np.save(f'./data/256/{name}_images_256x256.npy', np.array(images))
+    np.save(f'./data/256/{name}_images_grey_256x256.npy', np.array(images_grey))
 
 # Load npy files as a test
-test_images = np.load('./data/test_images.npy')
-test_images_grey = np.load('./data/test_images_grey.npy')
+test_images = np.load('./data/256/test_images_256x256.npy')
+test_images_grey = np.load('./data/256/test_images_grey_256x256.npy')
 
 # Make missing folders
-for path in ['./input/train/original', './input/train/grey',
-             './input/val/original', './input/val/grey',
-             './input/test/original', './input/test/grey']:
+for path in ['./input/256/train/original', './input/256/train/grey',
+             './input/256/val/original', './input/256/val/grey',
+             './input/256/test/original', './input/256/test/grey']:
     if not os.path.exists(path):
         os.makedirs(path)
 
-# Save all images as jpg
+# Save all images to input folder
 for i in range(len(test_images)):
-    plt.imsave(f'./input/test/original/{i}.jpg', test_images[i])
-    plt.imsave(f'./input/test/grey/{i}_grey.jpg', test_images_grey[i], cmap='gray')
-
-# Plot npy images to show theyre still correct
-fig, axis = plt.subplots(2, 5, figsize=(10, 5), sharex=True, sharey=True)
-for i in range(5):
-    plt.subplot(2, 5, i+1)
-    plt.imshow(test_images[i])
-    plt.subplot(2, 5, i+6)
-    plt.imshow(test_images_grey[i], cmap='gray')
-plt.show()
-
-# Save first five greyscale images as jpg
-for i in range(5):
-    plt.imsave(f'./images/greyscale{i}.jpg', test_images_grey[i], cmap='gray')
+    plt.imsave(f'./input/256/test/original/{i}.jpg', test_images[i])
+    plt.imsave(f'./input/256/test/grey/{i}_grey.jpg', test_images_grey[i], cmap='gray')
