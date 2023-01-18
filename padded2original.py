f0ec221a430508325e97bb25834cb8672cd24223
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-01-18 14:22:57 +0100

    Changed naming of fp

--- padded2original.py
+++ padded2original.py
@@ -6,12 +6,14 @@ from PIL import Image
 
 # Load a folder of padded images and save the original images
 # Restore resolution from 256x256 to 218x178 by cropping the zero-padded edges
-path = "./output"
-files = os.listdir(path)
+path_input = "./output/gan/output_untrained"
+path_output = "./unpadded/gan/output_untrained"
+
+files = os.listdir(path_input)
 files = [f for f in files if f.endswith(".jpg")]
 
 # Create a folder to save the original images
-os.makedirs("./unpadded", exist_ok=True)
+os.makedirs(path_output, exist_ok=True)
 
 # Calculate the mask for cropping the zero-padded edges
 new_width = int(178 / 218 * 256)  # All images were enlarged by (100 / 218 * 256) = 17%
@@ -22,7 +24,7 @@ right_bound = int((256 + new_width) / 2)
 # Loop over all images
 for i, file in enumerate(files):
     # Load image
-    image = Image.open(os.path.join(path, file))
+    image = Image.open(os.path.join(path_input, file))
     image = np.array(image)
 
     # Crop the zero-padded edges to restore the original aspect ratio
@@ -35,4 +37,4 @@ for i, file in enumerate(files):
 
     # Save image
     image = Image.fromarray(image)
-    image.save(os.path.join("./unpadded", file))
+    image.save(os.path.join(path_output, file))
