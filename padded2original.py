e058c93387297c37ee6143f2cbc0c099326b342d
StanMey <stan_1998@hotmail.com>
2023-01-20 14:04:36 +0100

    added parser arguments to file

--- padded2original.py
+++ padded2original.py
@@ -1,13 +1,22 @@
+# Load a folder of padded images and save the original images
+# Restores the resolution from 256x256 to 218x178 by cropping the zero-padded edges from images
+# Usage: python padded2original.py --path-input <path to padded images> --path-output <path to store unpadded images>
+# Example: python padded2original.py --path-input ./unpadded --path-output ./input/218/test/original
+
+import argparse
 import os
 
 import numpy as np
 import tensorflow as tf
 from PIL import Image
 
-# Load a folder of padded images and save the original images
-# Restore resolution from 256x256 to 218x178 by cropping the zero-padded edges
-path_input = "./output/gan/output_untrained"
-path_output = "./unpadded/gan/output_untrained"
+parser = argparse.ArgumentParser()
+parser.add_argument('--path-input', type=str, help='Path to folder of padded predicted images', default="./output/gan/output_untrained")
+parser.add_argument('--path-output', type=str, help='Path to folder to store the unpadded images', default="./unpadded/gan/output_untrained")
+args = parser.parse_args()
+
+path_input = args.path_input
+path_output = args.path_output
 
 files = os.listdir(path_input)
 files = [f for f in files if f.endswith(".jpg")]
